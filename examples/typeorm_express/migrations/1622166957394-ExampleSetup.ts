f2fd7971d1bd2df0cba6569c37cfd0d27614cd18
rl-gitpod <robert@gitpod.io>
2021-05-28 08:53:37 +0000

    Insert redirect URI correctly

--- examples/typeorm_express/migrations/1622166957394-ExampleSetup.ts
+++ examples/typeorm_express/migrations/1622166957394-ExampleSetup.ts
@@ -3,7 +3,7 @@ import {MigrationInterface, QueryRunner} from "typeorm";
 export class PostRefactoring1622166957394 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("INSERT INTO oauth_clients VALUES('ed883c20-bf58-11eb-8529-0242ac130003', 'not-a-secret', 'client example', '[http://localhost:76543]', 'authorization_code');");
+        await queryRunner.query("INSERT INTO oauth_clients VALUES('ed883c20-bf58-11eb-8529-0242ac130003', 'not-a-secret', 'client example', 'http://localhost:76543', 'authorization_code');");
         await queryRunner.query("INSERT INTO \"user\" VALUES ('d6d6708c-bf8f-11eb-8529-0242ac130003', 'example', 'user', 'user@example.com');");
     }
 
